3c3bce529dc95d968702cee04c4ca99678dd29b3
Seti-Ngabo <Seti-Ngabo@users.noreply.github.com>
2022-11-06 06:02:47 +0000

    Prettified Code!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
-};
+  plugins: ['react'],
+  rules: {}
+}
--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,7 @@ console.log('')
 const triangle1 = new Triangle(4, 5, 6)
 console.log('This is a 4 mm by 5 mm by 6 mm triangle.')
 console.log(' → Is the triangle valid: ' + triangle1.isValid().toString())
-console.log(
-  ' → Semiperimeter: ' + triangle1.semiPerimeter().toFixed(4) + ' mm'
-)
+console.log(' → Semiperimeter: ' + triangle1.semiPerimeter().toFixed(4) + ' mm')
 console.log(' → Area: ' + triangle1.area().toFixed(4) + ' mm²')
 console.log(' → Type: ' + triangle1.getType())
 console.log(' → Angle 1: ' + triangle1.angle(1).toFixed(4) + ' rad')
@@ -28,14 +26,10 @@ console.log(' → Height 1: ' + triangle1.height(1).toFixed(4) + ' mm')
 console.log(' → Height 2: ' + triangle1.height(2).toFixed(4) + ' mm')
 console.log(' → Height 3: ' + triangle1.height(3).toFixed(4) + ' mm')
 console.log(
-  ' → Inner circle radius: ' +
-    triangle1.innerCircleRadius().toFixed(4) +
-    ' mm'
+  ' → Inner circle radius: ' + triangle1.innerCircleRadius().toFixed(4) + ' mm'
 )
 console.log(
-  ' → Circumcircle radius: ' +
-    triangle1.circumsicleRadius().toFixed(4) +
-    ' mm'
+  ' → Circumcircle radius: ' + triangle1.circumsicleRadius().toFixed(4) + ' mm'
 )
 console.log('')
 
@@ -43,9 +37,7 @@ console.log('')
 const triangle2 = new Triangle(2, 5, 6)
 console.log('Created a 2 mm by 5 mm by 6 mm triangle.')
 console.log(' → Is the triangle valid: ' + triangle2.isValid().toString())
-console.log(
-  ' → Semiperimeter: ' + triangle2.semiPerimeter().toFixed(4) + ' mm'
-)
+console.log(' → Semiperimeter: ' + triangle2.semiPerimeter().toFixed(4) + ' mm')
 console.log(' → Area: ' + triangle2.area().toFixed(4) + ' mm²')
 console.log(' → Type: ' + triangle2.getType())
 console.log(' → Angle 1: ' + triangle2.angle(1).toFixed(4) + ' rad')
@@ -55,14 +47,10 @@ console.log(' → Height 1: ' + triangle2.height(1).toFixed(4) + ' mm')
 console.log(' → Height 2: ' + triangle2.height(2).toFixed(4) + ' mm')
 console.log(' → Height 3: ' + triangle2.height(3).toFixed(4) + ' mm')
 console.log(
-  ' → Inner circle radius: ' +
-    triangle2.innerCircleRadius().toFixed(4) +
-    ' mm'
+  ' → Inner circle radius: ' + triangle2.innerCircleRadius().toFixed(4) + ' mm'
 )
 console.log(
-  ' → Circumcircle radius: ' +
-    triangle2.circumsicleRadius().toFixed(4) +
-    ' mm'
+  ' → Circumcircle radius: ' + triangle2.circumsicleRadius().toFixed(4) + ' mm'
 )
 console.log('')
 
@@ -70,9 +58,7 @@ console.log('')
 const triangle3 = new Triangle(4, 4, 5)
 console.log('Created a 4 mm by 4 mm by 5 mm triangle.')
 console.log(' → Is the triangle valid: ' + triangle3.isValid().toString())
-console.log(
-  ' → Semiperimeter: ' + triangle3.semiPerimeter().toFixed(4) + ' mm'
-)
+console.log(' → Semiperimeter: ' + triangle3.semiPerimeter().toFixed(4) + ' mm')
 console.log(' → Area: ' + triangle3.area().toFixed(4) + ' mm²')
 console.log(' → Type: ' + triangle3.getType())
 console.log(' → Angle 1: ' + triangle3.angle(1).toFixed(4) + ' rad')
@@ -82,14 +68,10 @@ console.log(' → Height 1: ' + triangle3.height(1).toFixed(4) + ' mm')
 console.log(' → Height 2: ' + triangle3.height(2).toFixed(4) + ' mm')
 console.log(' → Height 3: ' + triangle3.height(3).toFixed(4) + ' mm')
 console.log(
-  ' → Inner circle radius: ' +
-    triangle3.innerCircleRadius().toFixed(4) +
-    ' mm'
+  ' → Inner circle radius: ' + triangle3.innerCircleRadius().toFixed(4) + ' mm'
 )
 console.log(
-  ' → Circumcircle radius: ' +
-    triangle3.circumsicleRadius().toFixed(4) +
-    ' mm'
+  ' → Circumcircle radius: ' + triangle3.circumsicleRadius().toFixed(4) + ' mm'
 )
 console.log('')
 
@@ -97,9 +79,7 @@ console.log('')
 const triangle4 = new Triangle(3, 3, 4)
 console.log('Created a 3 mm by 3 mm by 4 mm triangle.')
 console.log(' → Is the triangle valid: ' + triangle4.isValid().toString())
-console.log(
-  ' → Semiperimeter: ' + triangle4.semiPerimeter().toFixed(4) + ' mm'
-)
+console.log(' → Semiperimeter: ' + triangle4.semiPerimeter().toFixed(4) + ' mm')
 console.log(' → Area: ' + triangle4.area().toFixed(4) + ' mm²')
 console.log(' → Type: ' + triangle4.getType())
 console.log(' → Angle 1: ' + triangle4.angle(1).toFixed(4) + ' rad')
@@ -109,14 +89,10 @@ console.log(' → Height 1: ' + triangle4.height(1).toFixed(4) + ' mm')
 console.log(' → Height 2: ' + triangle4.height(2).toFixed(4) + ' mm')
 console.log(' → Height 3: ' + triangle4.height(3).toFixed(4) + ' mm')
 console.log(
-  ' → Inner circle radius: ' +
-    triangle4.innerCircleRadius().toFixed(4) +
-    ' mm'
+  ' → Inner circle radius: ' + triangle4.innerCircleRadius().toFixed(4) + ' mm'
 )
 console.log(
-  ' → Circumcircle radius: ' +
-    triangle4.circumsicleRadius().toFixed(4) +
-    ' mm'
+  ' → Circumcircle radius: ' + triangle4.circumsicleRadius().toFixed(4) + ' mm'
 )
 console.log('')
 
@@ -124,9 +100,7 @@ console.log('')
 const triangle5 = new Triangle(23, 3, 4)
 console.log('Created a 23 mm by 3 mm by 4 mm triangle.')
 console.log(' → Is the triangle valid: ' + triangle5.isValid().toString())
-console.log(
-  ' → Semiperimeter: ' + triangle5.semiPerimeter().toFixed(4) + ' mm'
-)
+console.log(' → Semiperimeter: ' + triangle5.semiPerimeter().toFixed(4) + ' mm')
 console.log(' → Area: ' + triangle5.area().toFixed(4) + ' mm²')
 console.log(' → Type: ' + triangle5.getType())
 console.log(' → Angle 1: ' + triangle5.angle(1).toFixed(4) + ' rad')
@@ -136,14 +110,10 @@ console.log(' → Height 1: ' + triangle5.height(1).toFixed(4) + ' mm')
 console.log(' → Height 2: ' + triangle5.height(2).toFixed(4) + ' mm')
 console.log(' → Height 3: ' + triangle5.height(3).toFixed(4) + ' mm')
 console.log(
-  ' → Inner circle radius: ' +
-    triangle5.innerCircleRadius().toFixed(4) +
-    ' mm'
+  ' → Inner circle radius: ' + triangle5.innerCircleRadius().toFixed(4) + ' mm'
 )
 console.log(
-  ' → Circumcircle radius: ' +
-    triangle5.circumsicleRadius().toFixed(4) +
-    ' mm'
+  ' → Circumcircle radius: ' + triangle5.circumsicleRadius().toFixed(4) + ' mm'
 )
 
 // Output
